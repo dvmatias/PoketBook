fa9f29407ea746eb74b1b3d0eee3f1462d86c1cb
Matías <matias.delv.dom@gmail.com>
2020-12-18 22:47:03 -0300

    Code clean up

--- feature-landing/src/main/java/com/cmdv/feature_landing/ui/ui/home/OpenBooksFragment.kt
+++ feature-landing/src/main/java/com/cmdv/feature_landing/ui/ui/home/OpenBooksFragment.kt
@@ -5,25 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.fragment.app.viewModels
 import com.cmdv.feature_landing.R
 
 class OpenBooksFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private val viewModel: OpenBooksViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_open_books, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_home)
-//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
+
         return root
     }
 }
