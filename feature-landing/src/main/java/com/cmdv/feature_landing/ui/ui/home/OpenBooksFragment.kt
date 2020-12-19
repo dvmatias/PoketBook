32bc8006dbca008e33e005eb3b912cce399b643b
Matías <matias.delv.dom@gmail.com>
2020-12-18 21:30:16 -0300

    Added open Book Fragment

--- feature-landing/src/main/java/com/cmdv/feature_landing/ui/ui/home/OpenBooksFragment.kt
+++ feature-landing/src/main/java/com/cmdv/feature_landing/ui/ui/home/OpenBooksFragment.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.cmdv.feature_landing.R
 
-class HomeFragment : Fragment() {
+class OpenBooksFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
@@ -21,11 +19,11 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val root = inflater.inflate(R.layout.fragment_open_books, container, false)
+//        val textView: TextView = root.findViewById(R.id.text_home)
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
 }
